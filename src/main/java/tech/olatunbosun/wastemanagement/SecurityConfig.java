37df4ba37106a7e079344f23b4c37e8d012e437c
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-02-24 22:21:08 +0100

    feat: working on spring security

--- src/main/java/tech/olatunbosun/wastemanagement/SecurityConfig.java
+++ src/main/java/tech/olatunbosun/wastemanagement/SecurityConfig.java
@@ -1,4 +1,23 @@
 package tech.olatunbosun.wastemanagement;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("v1/user/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 }
