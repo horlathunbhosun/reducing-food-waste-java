d572465e0c775e47edb435c0f9d8d8e0ba7736ce
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-06-08 13:26:53 +0200

    feat: added some files to config

--- startrbmq.sh
+++ startrbmq.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 export RABBITMQ_USERNAME=myuser
 export RABBITMQ_PASSWORD=mypassword
+export SPRING_DATASOURCE_URL=jdbc:sqlserver://wastedbb.database.windows.net:1433;database=wastedb;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+export SPRING_DATASOURCE_USERNAME=AdminOla@wastedbb
+export SPRING_DATASOURCE_PASSWORD=Olatunbosun02@
+
+docker build -t my-app .
 
 docker compose up
