ca35b8c8a5f3a064f6880f28fea737d7654ef247
Olulode Olatunbosun <olulodeolatunbosun@yahoo.com>
2024-09-11 18:20:43 +0200

    Update Token.java
    Removd Space

--- src/main/java/tech/olatunbosun/wastemanagement/usermanagement/models/Token.java
+++ src/main/java/tech/olatunbosun/wastemanagement/usermanagement/models/Token.java
@@ -14,17 +14,12 @@ public class Token {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer id;
-
     @Column(unique = true)
     public String token;
-
     @Enumerated(EnumType.STRING)
     public TokenType tokenType = TokenType.BEARER;
-
     public boolean revoked;
-
     public boolean expired;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     public User user;
